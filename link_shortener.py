386f8db7d5b81af743a574f86942154a8c4e6757
[Alexandr] <[afominyh242@gmail.com]>
2022-04-24 18:44:16 +0700

    Code refactoring, renamed response headers.

--- link_shortener.py
+++ link_shortener.py
@@ -8,39 +8,39 @@ import requests
 from dotenv import load_dotenv
 
 
-def check_url_accessibility(url: str) -> bool:
-    '''Function checks url for avaliblity'''
+def check_url_accessibility(url: str) -> None:
+    """Checks url for availability"""
     server_answer = requests.get(url)
-    return server_answer.raise_for_status()
+    server_answer.raise_for_status()
 
 
-def is_bitlink(token: str, url: str) -> str:
-    '''The function returns the number of clicks on the bitlink.
-    If there is no such bitlink, the bitlink will be returned.'''
+def is_bitlink(header: dict, url: str) -> str:
+    """Returns the number of clicks on the bit-link.
+    If there is no such bit-link, the bit-link will be returned."""
 
     api_link = 'https://api-ssl.bitly.com/v4/bitlinks/{}'
     request_link = api_link.format(urlparse(url).netloc + urlparse(url).path)
-    bitly_response = requests.get(request_link, headers=token)
+    bitly_response = requests.get(request_link, headers=header)
     return bitly_response.ok
 
 
-def get_click_count(token: str, url: str) -> int:
-    '''Function returns click count all time for bitlink'''
+def get_click_count(header: dict, url: str) -> int:
+    """Returns click count all time for bitlink"""
 
     api_link = 'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'
     request_link = api_link.format(urlparse(url).netloc + urlparse(url).path)
-    bitly_response = requests.get(request_link, headers=token)
+    bitly_response = requests.get(request_link, headers=header)
     bitly_response.raise_for_status()
     return bitly_response.json()['total_clicks']
 
 
-def get_shorten_link(token: str, url: str) -> str:
-    '''Function returns shortern link'''
+def get_shorten_link(header: dict, url: str) -> str:
+    """Returns shortern link"""
 
     data = {'long_url': url}
     bitly_response = requests.post(
             'https://api-ssl.bitly.com/v4/bitlinks',
-            headers=token,
+            headers=header,
             json=data
     )
     bitly_response.raise_for_status()
@@ -51,16 +51,16 @@ if __name__ == '__main__':
 
     env_path = Path('.') / '.env'
     load_dotenv(env_path)
-    bitlink_token = os.getenv('BITLINK_TOKEN')
+    bitlink_header = os.getenv('BITLINK_TOKEN')
 
     user_input = input(f'Paste bitlink/url here: ').strip()
     authorization_header = {'Authorization': f'Bearer {bitlink_token}'}
     try:
         check_url_accessibility(url=user_input)
-        if is_bitlink(token=authorization_header, url=user_input):
-                print(f'Clicks Count: {get_click_count(token=authorization_header, url=user_input)}')
+        if is_bitlink(header=authorization_header, url=user_input):
+            print(f'Clicks Count: {get_click_count(header=authorization_header, url=user_input)}')
         else:
-                print(f'Shorten Link: {get_shorten_link(token=authorization_header, url=user_input)}')
+            print(f'Shorten Link: {get_shorten_link(header=authorization_header, url=user_input)}')
     except requests.exceptions.ConnectionError:
         print('ConnectionError: can\'t connect to server.')
     except requests.exceptions.HTTPError:
